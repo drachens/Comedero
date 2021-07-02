34e319ac691762066974b81d919588ad853f170b
drachens <68623221+drachens@users.noreply.github.com>
2021-07-02 03:53:55 -0400

    Delete dinamic directory

--- dinamic/borrarProducto.php
+++
@@ -1,7 +0,0 @@
-<?php
-	$idRemove = $_GET['id'];
-	session_start();
-	unset($_SESSION['carrito'][$idRemove]);
-	print_r($_SESSION['carrito']);
-	header("Location: /dinamic/carrito.php");
-  ?>
--- dinamic/pruebas.php
+++
@@ -1,6 +0,0 @@
-<?php
-	session_start();
-	$nombre = session_id();
-	echo $nombre;
-
-?>
